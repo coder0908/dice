de763ed88c3bed87fcef2749a31c36576b1f7c2b
coder0908 <da471283@gmail.com>
2026-02-10 12:08:55 +0000

    Change assembly programme

--- dasm/test/main.c
+++ dasm/test/main.c
@@ -2,17 +2,30 @@
 #include <dasm.h>
 #include <stdio.h>
 
+#define  PROGRAMME "	label0: nop		\n	\
+			iadd 111 *111 #1	\n	\
+			igt 112 *111 #8		\n 	\
+			puti *111 		\n 	\
+			jmpz *112 *label0	\n	\
+			eop"
+
+
 int main(void)
 {	
 	libdice_word_t exe[LIBDASM_PROGRAMME_MAX_LEN] = {0,};
 	libdice_word_t exe_cnt = 0;
 	libdice_word_t i = 0;
 
-	char asm_programme[LIBDASM_PROGRAMME_MAX_LEN] = "	buf: nop\n	iadd buf #2 #1\n	puti *buf\n eop	";
+	char asm_programme[LIBDASM_PROGRAMME_MAX_LEN] = PROGRAMME;
 	exe_cnt = libdasm_assemble(exe, LIBDASM_PROGRAMME_MAX_LEN, asm_programme, LIBDASM_PROGRAMME_MAX_LEN);
-	
-	for (i=0; i<exe_cnt; ++i) {
-		printf("%u\n", exe[i]);
+	if (exe_cnt == LIBDASM_ERR_RET) {
+		printf("Syntax error\n");
+		return 0;
 	}
+
+	for (i=0; i<exe_cnt; ++i) {
+		printf("%u ", exe[i]);
+	}
+	printf("\n");
 	return 0; 
-} 
+} 	
